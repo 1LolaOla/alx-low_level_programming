2c618eca2aa6206577289acf0234d1a561cc3b5f
1LolaOla <funmiolay1@gmail.com>
2022-10-18 21:11:15 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,12 +14,12 @@ void times table(void)
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
-			_putchar(' ');
+			_putchar('');
 
 			prod = num * mult;
 
 			if (prod <= 9)
-				_putchar(' ');
+				_putchar('');
 			else
 				_putchar((prod / 10) + '0');
 
