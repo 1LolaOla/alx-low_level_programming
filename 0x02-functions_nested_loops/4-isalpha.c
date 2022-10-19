32e79d98b530a86ca0c87091708979d31dd12bb0
1LolaOla <funmiolay1@gmail.com>
2022-10-18 17:56:13 -0700

    4 isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _isalpha -checks for alphabets
+ * _isalpha -checks for char alphabet
  * @c: character to be checked
  *
- * Return: 1 if character is a letter, 0 otherwise
+ * Return: 1 if character is a letter lower or upper, 0 otherwise
  */
 
 int _isalpha(int c)
