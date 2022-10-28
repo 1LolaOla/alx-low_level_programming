72433030e62c8cc643657e407c2ebfa4342dac01
1LolaOla <funmiolay1@gmail.com>
2022-10-28 00:09:28 -0700

    104 print buffer c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
-/*
+/**
  * print_line - prints a s bytes of a buffer
  * @c: buffer to print
  * @s: bytes of buffer to print
@@ -32,7 +32,7 @@ void print_line(char *c, int s, int l)
 	}
 }
 
-/*
+/**
  * print_buffer - prints a buffer
  * @b: buffer to print
  * @size: size of buffer
