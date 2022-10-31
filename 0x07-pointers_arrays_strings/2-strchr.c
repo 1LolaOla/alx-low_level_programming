9c6fc4dab0071ec08b0286c8ad7613dd2e673e50
1LolaOla <funmiolay1@gmail.com>
2022-10-31 13:00:59 -0700

    2 strchr c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,9 +6,8 @@
  * @c: The character to be located.
  *
  * Return: If c is found - a pointer to the first occurence.
- * If c is not found - NULL.
  */
-char *_strchr(char *s, char c);
+char *_strchr(char *s, char c)
 {
 	while (*s)
 	{
