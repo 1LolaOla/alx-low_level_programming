02c43301f2a15c60b770eac5bdb3b685fb3beb87
1LolaOla <funmiolay1@gmail.com>
2022-11-05 04:41:50 -0700

    1 args c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Function that prints the number of arguments it received
+ * @argc: number of arguments
+ * @argv: elements
+ *
+ * Return: void (empty)
+ */
+int main(int argc, __attribute__((unused)) char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+
