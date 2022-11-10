c13c2b3aab61d483a14274a0ce40c6e51a94d266
1LolaOla <funmiolay1@gmail.com>
2022-11-10 00:46:21 -0800

    updated

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+int main(void)
+{
+	_puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
