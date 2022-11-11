b433257624c0667d2e0d3a3c33f106440c1b571c
1LolaOla <funmiolay1@gmail.com>
2022-11-10 20:15:38 -0800

    update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
 
-/**
- * File: main.h
- */
-
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
