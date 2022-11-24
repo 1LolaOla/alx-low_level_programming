620be67552f3af512da0d05f54e355893159f3ee
1LolaOla <funmiolay1@gmail.com>
2022-11-24 03:23:57 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * hair - comes before main
+ * @void - no params
+ *
+ * Return: void
+ */
+
+#include "lists.h"
+
+void hair(void)
+{
+	printf("You\'re beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
+}
