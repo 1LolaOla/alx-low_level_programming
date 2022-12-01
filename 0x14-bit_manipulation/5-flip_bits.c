eb0d9905331809a71a3eaca221a67b5f94eabfdf
1LolaOla <funmiolay1@gmail.com>
2022-12-01 13:04:01 -0800

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,12 +10,12 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int differnce, result;
+	unsigned long int difference, result;
 	unsigned int j, i;
 
 	j = 0;
 	result = 1;
-	differnce = n ^ m;
+	difference = n ^ m;
 	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
 	{
 		if (result == (difference & result))
