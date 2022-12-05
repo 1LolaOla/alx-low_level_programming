056296bbebcb5851a5ee698c10bd07082ee1d907
1LolaOla <funmiolay1@gmail.com>
2022-12-05 14:29:24 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,111 +1,120 @@
 #include "main.h"
-
-void write_error_check(int writtenBytes, char *args[], int fd_from);
-void read_error_check(int readBytes, char *args[]);
-void close_error_check(int fd);
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - entry
- * @argc: arg count
- * @args: command line args
+ * check97 - checks for the correct number of arguments
+ * @argc: number of arguments
  *
- * Return: int
+ * Return: void
  */
 
-int main(int argc, char **args)
+void check97(int argc)
 {
-	int fd_to = 0, fd_from = 0, writtenBytes = 0, readBytes = 0;
-	char buffer[1024];
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	fd_from = open(args[1], O_RDONLY);
-
-	if (fd_from < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", args[1]);
-		exit(98);
-	}
-
-	fd_to = open(args[2], O_WRONLY | O_CREAT | O_TRUNC, 00664);
-
-	if (fd_to < 0)
-	{
-		printf(STDERR_FILENO, "Error: Can't write to %s\n", args[2]);
-		exit(99);
-	}
-	while ((readBytes = read(fd_from, buffer, 1024)) > 0)
-	{
-		writtenBytes = write(fd_to, buffer, readBytes);
-		write_error_check(writtenBytes, args, fd_from);
-	}
-
-	read_error_check(readBytes, args);
-	close_error_check(fd_from);
-	close_error_check(fd_to);
-
-	return (0);
 }
 
 /**
- * write_error_check - checks write errors
- * @writtenbytes: bytes written
- * @args: command line args
- * @fd_from: file descriptor for file to copy from
+ * check98 - checks that file_from exists and can be read
+ * @check: checks if true of false
+ * @file: file_from name
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
  *
  * Return: void
  */
 
-void write_error_check(int writtenbytes, char *args[], int fd_from)
+void check98(ssize_t check, char *file, int fd_from, int fd_to)
 {
-	if (writtenbytes < 0)
+	if (check == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", args[2]);
-		close_error_check(fd_from);
-		exit(99);
-	}
-}
-
-/**
- * read_error_check - checks read errors
- * @readbytes: bytes read
- * @args: command line args
- *
- * Return: void
- */
-
-void read_error_check(int readbytes, char *args[])
-{
-	if (readbytes < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", args[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
 		exit(98);
 	}
 }
 
 /**
- * close_error_check - checks close errors
+ * check99 - checks that file_to was created and/or can be written to
+ * @check: checks if true of false
+ * @file: file_to name
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
+ *
+ * Return: void
+ */
+
+void check99(ssize_t check, char *file, int fd_from, int fd_to)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(99);
+	}
+}
+
+/**
+ * check100 - checks that file descriptors were closed properly
+ * @check: checks if true or false
  * @fd: file descriptor
  *
  * Return: void
  */
 
-void close_error_check(int fd)
-
+void check100(int check, int fd)
 {
-	if (close(fd) < 0)
+	if (check == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
 
+/**
+ * main - opies the content of a file to another file.
+ * @argc: number of arguments passed
+ * @argv: array of pointers to the arguments
+ *
+ * Return: 0 on success
+ */
 
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, close_to, close_from;
+	ssize_t lenr, lenw;
+	char buffer[1024];
+	mode_t file_perm;
 
-
-
-
+	check97(argc);
+	fd_from = open(argv[1], O_RDONLY);
+	check98((ssize_t)fd_from, argv[1], -1, -1);
+	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
+	check99((ssize_t)fd_to, argv[2], fd_from, -1);
+	lenr = 1024;
+	while (lenr == 1024)
+	{
+		lenr = read(fd_from, buffer, 1024);
+		check98(lenr, argv[1], fd_from, fd_to);
+		lenw = write(fd_to, buffer, lenr);
+		if (lenw != lenr)
+			lenw = -1;
+		check99(lenw, argv[2], fd_from, fd_to);
+	}
+	close_to = close(fd_to);
+	close_from = close(fd_from);
+	check100(close_to, fd_to);
+	check100(close_from, fd_from);
+	return (0);
+}
