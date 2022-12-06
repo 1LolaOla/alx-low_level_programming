c9e7f3eea3cc3f251be0aedf64cc1debaa769428
1LolaOla <funmiolay1@gmail.com>
2022-12-05 16:59:44 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -42,7 +42,7 @@ void close_file(int fd)
 	if (c == -1)
 	{
 
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
