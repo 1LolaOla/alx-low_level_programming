eaef231c6e6a662618af16a2fb36558cbb5abf85
1LolaOla <funmiolay1@gmail.com>
2023-01-12 09:08:49 -0800

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -29,7 +29,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	while (temp->next != NULL)
 	{
-		emp = temp->next;
+		temp = temp->next;
 	}
 	temp->next = new_node;
 	new_node->prev = temp;
