2d3ccf4a4bdf0df94f8777f48db5a5226fef5458
1LolaOla <funmiolay1@gmail.com>
2023-02-05 01:00:34 -0800

    0-hash_table_create.c

--- 0-hash_table_create.c/0-hash_table_create.c
+++ 0-hash_table_create.c/0-hash_table_create.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - hash_table_create
- * @size: size of array in hash table
+ * hash_table_create - creates a hash table
+ * @size: size of the array
  *
- * Return: pointer to hash table
+ * Return: pointer to the newly created hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
